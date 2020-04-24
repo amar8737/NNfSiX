3113badd1aba73d5f4f31f19a04027f481d65cdf
AMAR JEET KUSHWAHA <babuamar455@gmail.com>
2020-04-24 15:45:05 +0530

    Update p001-Basic-Neuron-3-inputs.py

--- Python/p001-Basic-Neuron-3-inputs.py
+++ Python/p001-Basic-Neuron-3-inputs.py
@@ -4,9 +4,12 @@ Creates a basic neuron with 3 inputs.
 Associated YT NNFS tutorial: https://www.youtube.com/watch?v=Wo5dMEP_BbI
 '''
 
-inputs = [1.2, 5.1, 2.1]
-weights = [3.1, 2.1, 8.7]
-bias = 3.0
-
-output = inputs[0]*weights[0] + inputs[1]*weights[1] + inputs[2]*weights[2] + bias
+import numpy as np
+inputs = [1,2,3,2.5]
+weights =[[0.2,0.8,-0.5,1.0],
+            [0.5,-0.91,0.26,-0.5],
+            [-0.26,-0.27,0.17,0.87]
+          ]
+bais = [2,3,0.5]
+output = np.dot(weights,inputs)+bais
 print(output)
